37ef0d538915e964ead40e503ded269fa76c1351
RadSham <radsham9@gmail.com>
2023-08-03 21:17:22 +0300

    TabLayout: TabRow & HorizontalPager

--- app/src/main/java/com/example/weatherappcompose/MainActivity.kt
+++ app/src/main/java/com/example/weatherappcompose/MainActivity.kt
@@ -3,7 +3,16 @@ package com.example.weatherappcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.weatherappcompose.screens.MainScreen
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Tab
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.example.weatherappcompose.screens.MainCard
+import com.example.weatherappcompose.screens.TabLayout
 import com.example.weatherappcompose.ui.theme.WeatherAppComposeTheme
 
 const val API_KEY = "0570ad2fb7e546ec815183456232407"
@@ -13,7 +22,19 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WeatherAppComposeTheme {
-                MainScreen()
+                Image(
+                    painter = painterResource(id = R.drawable.backgound_clouds),
+                    contentDescription = "im1",
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .alpha(0.7f),
+                    contentScale = ContentScale.FillBounds
+                )
+                Column {
+                    MainCard()
+                    TabLayout()
+                }
+
             }
         }
     }
